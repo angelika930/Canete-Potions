99d929c06b92b295b9a2a52262c377503140bc28
Angelika Canete <52233077+angelika930@users.noreply.github.com>
2024-10-07 18:23:45 -0700

    buy blue barrels

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -79,10 +79,10 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
     
     desired_barrel = ""
 
-    if row.num_red_potions == 0:
+    if row.num_blue_potions == 0:
         desired_barrel = "red"
     
-    elif row.num_blue_potions == 0:
+    elif row.num_red_potions == 0:
         desired_barrel = "blue"
         
 
